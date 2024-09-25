4871660fdd046f098b61165d2eadaa91f8ab686e
Musaddek-Ahmed <musaddek59@gmail.com>
2024-09-25 19:45:40 +0600

    latest

--- app/components/feature.tsx
+++ app/components/feature.tsx
@@ -2,11 +2,57 @@
 
 import React from 'react'
 import { Element } from 'react-scroll'
-import {details, features } from '../constants/route'
 import Image from 'next/image'
 import Button from './button'
 
 const Feature = () => {
+    const features = [
+        {
+          id: '0',
+          icon: '/images/feature-1.png',
+          caption: 'Easy integration',
+          title: 'Work smarter not harder',
+          text: "With Xora, tedious tasks are history. Automation and smart processes bring your productivity to new heights. It's like having an extra cup of coffee, but without the jitters.",
+          button: {
+            icon: '/images/magictouch.svg',
+            title: 'Watch the demo',
+          },
+        },
+        {
+          id: '1',
+          icon: '/images/feature-2.png',
+          caption: 'Secure & trustworthy',
+          title: 'Sleep easy, we got your back',
+          text: 'Your data security is our priority. With state-of-the-art encryption and robust privacy controls, Xora helps keeps your information secure and locked up tighter than Fort Knox.',
+          button: {
+            icon: '/images/docs.svg',
+            title: 'Read the docs',
+          },
+        },
+      ];
+
+      const details = [
+        {
+          id: '0',
+          icon: '/images/detail-1.png',
+          title: 'AI automated video editing',
+        },
+        {
+          id: '1',
+          icon: '/images/detail-2.png',
+          title: 'Collaborate with your team',
+        },
+        {
+          id: '2',
+          icon: '/images/detail-3.png',
+          title: 'Ultra fast cloud-engine',
+        },
+        {
+          id: '3',
+          icon: '/images/detail-4.png',
+          title: '24 / 7 Customer support',
+        },
+      ];
   return (
     <section>
         <Element name='features'>
