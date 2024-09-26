07ad6e34d711993b9a2b187248bce4199598b3ee
Musaddek-Ahmed <musaddek59@gmail.com>
2024-09-26 12:03:17 +0600

    latest update pricing section updated

--- app/components/constant.tsx
+++ app/components/constant.tsx
@@ -163,53 +163,7 @@
   //   },
   // ];
   
-  // export const plans = [
-  //   {
-  //     id: '0',
-  //     title: 'Core',
-  //     priceMonthly: 19,
-  //     priceYearly: 12,
-  //     caption: 'Best for solo creators',
-  //     features: [
-  //       '100MB Cloud storage',
-  //       '100+ prompt templates',
-  //       '5 projects',
-  //       '24/7 support',
-  //     ],
-  //     icon: '/images/circle.svg',
-  //     logo: '/images/plan-1.png',
-  //   },
-  //   {
-  //     id: '1',
-  //     title: 'Overdrive',
-  //     priceMonthly: 79,
-  //     priceYearly: 59,
-  //     caption: 'Most popular plan',
-  //     features: [
-  //       'All Starter features',
-  //       '1TB additional storage',
-  //       'Unlimited projects',
-  //       'Analytics',
-  //     ],
-  //     icon: '/images/triangle.svg',
-  //     logo: '/images/plan-2.png',
-  //   },
-  //   {
-  //     id: '2',
-  //     title: 'Team',
-  //     priceMonthly: 39,
-  //     priceYearly: 29,
-  //     caption: 'Exclusively for teams',
-  //     features: [
-  //       'All Overdrive features',
-  //       '10TB additional storage',
-  //       '50% off per member',
-  //       'Real-time collaboration',
-  //     ],
-  //     icon: '/images/hexagon.svg',
-  //     logo: '/images/plan-3.png',
-  //   },
-  // ];
+
   
   // export const testimonials = [
   //   {
--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import Image from "next/image";
 import Header from "./components/header";
 import Hero from "./components/hero";
 import Feature from "./components/feature";
+import Pricing from "./components/pricing";
 
 export default function Home() {
   return (
@@ -9,6 +10,7 @@ export default function Home() {
 <Header/>
 <Hero/>
 <Feature/>
+<Pricing/>
 </div>
   );
 }
